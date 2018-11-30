ddcd9f2b41e29911cff9e923d235b6a7bed67a02
时广玉 <shiguangyu@dlaotianhuang.com>
2018-11-30 16:48:18 +0800

    Optimizing Module Generating class

--- shell.sh
+++ shell.sh
@@ -2,30 +2,58 @@ git status
 
 git add .
 
+git status
+
 git commit -m 'Optimizing Module Generating class'
+
+git status
 
 git push mayun CheShi_1
 
+git status
+
 git push gitlab CheShi_1
+
+git status
 
 git push github CheShi_1
 
+git status
+
 git checkout CheShi_1
+
+git status
 
 git push mayun ShiGuangYu
 
+git status
+
 git push gitlab ShiGuangYu
+
+git status
 
 git push github ShiGuangYu
 
+git status
+
 git checkout master
+
+git status
 
 git merge ShiGuangYu
 
+git status
+
 git push mayun master
+
+git status
 
 git push gitlab master
 
+git status
+
 git push github master
+
+git status
 
 git checkout CheShi_1
